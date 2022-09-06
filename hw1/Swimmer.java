69c93b2e819782ee9ad9e621af0ee5b4d3a9ec27
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2022-09-06 11:23:26 -0700

    Add files via upload

---
+++ hw1/Swimmer.java
@@ -0,0 +1,12 @@
+/**
+ @author: Justin Yamamoto
+ @date: 9/6/2022
+
+ This is part of exercise 5 of cs151 hw1 with professor newton
+ This implements the Swimmer interface
+*/
+
+public interface Swimmer
+{
+  void swim();
+}
