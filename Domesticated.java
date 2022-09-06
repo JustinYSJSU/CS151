30eb7b8a76d1eea567800be51bf71dd1041a65c2
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2022-09-06 11:22:14 -0700

    Add files via upload

---
+++ Domesticated.java
@@ -0,0 +1,13 @@
+/**
+ @author: Justin Yamamoto
+ @date: 9/6/2022
+
+ This is part of exercise 5 of cs151 hw1 with professor Newton
+ This implements the Domesticated interface
+*/
+
+public interface Domesticated
+{
+   void walk();
+   void greetHuman();
+}
