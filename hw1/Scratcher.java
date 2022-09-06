c97fbd66fdeed11756c52532e3e11327dbcb219d
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2022-09-06 11:38:28 -0700

    Add files via upload

---
+++ hw1/Scratcher.java
@@ -0,0 +1,11 @@
+/**
+ @author: Justin Yamamoto
+ @date: 9/6/2022
+
+ This is part of exercise 5 of cs151 hw1 with professor newton
+ This implements the Scratcher interface
+*/
+public interface Scratcher
+{
+  void scratch();
+}
